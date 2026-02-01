88e9a6f3cceed1aea4be1ea4bac18672520bd7ae
Mukarillo <mukarillo@gmail.com>
2026-02-01 01:12:08 +0100

    refactor card service

---
+++ Assets/Scripts/Card/Card.cs
@@ -0,0 +1,14 @@
+﻿namespace Engine
+{
+    public class Card
+    {
+        public int CardId { get; private set; }
+        public byte[] Data { get; private set; }
+
+        public Card(int cardId, byte[] data)
+        {
+            CardId = cardId;
+            Data = data;
+        }
+    }
+}
--- Assets/Scripts/Card/CardsService.cs
+++ Assets/Scripts/Card/CardsService.cs
@@ -7,13 +7,32 @@ namespace Engine
         public static CardsService Instance => instance ??= new CardsService();
         private static CardsService instance;
 
-        public byte[][] MaskCards { get; private set; }
-        public byte[][] PlayerCards { get; private set; }
+        public Card[] MaskCards { get; private set; }
+        public Card[] PlayerCards { get; private set; }
         
         public void InjectCardsData(CardsDataDto csd)
         {
-            MaskCards = csd.MaskCards;
-            PlayerCards = csd.PlayerCards;
+            MaskCards = new Card[csd.MaskCards.Length];
+            for (var i = 0; i < csd.MaskCards.Length; i++)
+                MaskCards[i] = new Card(i, csd.MaskCards[i]);
+            
+            PlayerCards = new Card[csd.PlayerCards.Length];
+            for (var i = 0; i < csd.PlayerCards.Length; i++)
+                PlayerCards[i] = new Card(i, csd.PlayerCards[i]);
+        }
+
+        public Card GetPlayerCardWithId(int cardId)
+        {
+            if (cardId < 0 || cardId >= MaskCards.Length)
+                return null;
+            return PlayerCards[cardId];
+        }
+
+        public Card GetMaskCardWithId(int cardId)
+        {
+            if (cardId < 0 || cardId >= MaskCards.Length)
+                return null;
+            return MaskCards[cardId];
         }
     }
 }
