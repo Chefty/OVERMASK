9636c952c50fb8671962b11ba1da4c226ed6e20c
XiaoWei Victor Qian <victorfreshstart@gmail.com>
2026-02-01 02:04:44 +0100

    update build config

--- Assets/Scripts/Playmat/PlaymatView.cs
+++ Assets/Scripts/Playmat/PlaymatView.cs
@@ -3,10 +3,16 @@ using UnityEngine;
 
 public class PlaymatView : MonoBehaviour
 {
+    public static PlaymatView Instance;
     [SerializeField] private TMP_Text playerName;
     [SerializeField] private TMP_Text opponentName;
     public PlayerSlot playerSlot;
     public OpponentSlot opponentSlot;
     public HouseSlot houseSlot;
     public CombatSlot combatSlot;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 }
