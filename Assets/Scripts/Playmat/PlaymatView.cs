5b526d6ed33dd526cd1d6d6d08958427c37858d3
Mukarillo <mukarillo@gmail.com>
2026-02-01 12:16:43 +0100

    change opponent and player references to blue and red

--- Assets/Scripts/Playmat/PlaymatView.cs
+++ Assets/Scripts/Playmat/PlaymatView.cs
@@ -1,21 +1,19 @@
 using TMPro;
 using UnityEngine;
-using client;
 using DG.Tweening;
 
 public class PlaymatView : MonoBehaviour
 {
     public static PlaymatView Instance;
-    [SerializeField] private TMP_Text playerName;
-    [SerializeField] private TMP_Text opponentName;
-    [SerializeField] private TMP_Text playerScore;
-    [SerializeField] private TMP_Text opponentScore;
+    [SerializeField] private TMP_Text blueName;
+    [SerializeField] private TMP_Text redName;
+    [SerializeField] private TMP_Text blueScore;
+    [SerializeField] private TMP_Text redScore;
     [SerializeField] private TMP_Text roundText;
     [SerializeField] private TMP_Text readyText;
-    public PlayerSlot playerSlot;
-    public OpponentSlot opponentSlot;
+    public PlayerSlot blueSlot;
+    public OpponentSlot redSlot;
     public CombatSlot combatSlot;
-    public OpponentCardDisplayer opponentCardDisplayer;
     public HouseCardDisplayer houseCardDisplayer;
 
     private int roundNumber = 0;
@@ -27,14 +25,14 @@ public class PlaymatView : MonoBehaviour
 
     private void Start()
     {
-        playerName.text = Game.Instance.Round.LocalPlayer.Name;
-        opponentName.text = Game.Instance.Round.OpponentPlayer.Name;
+        blueName.text = Game.Instance.Round.LocalPlayer.Name;
+        redName.text = Game.Instance.Round.OpponentPlayer.Name;
     }
     
     public void UpdateScores(int playerScoreValue, int opponentScoreValue)
     {
-        playerScore.text = playerScoreValue.ToString();
-        opponentScore.text = opponentScoreValue.ToString();
+        blueScore.text = playerScoreValue.ToString();
+        redScore.text = opponentScoreValue.ToString();
     }
     
     public void UpdateRoundText()
